c279a902ec0add51d2a4b04d2494ee1ee070426b
shiba0627 <1E8EC3@gmail.com>
2025-07-18 05:44:54 +0900

    v1.2

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
 
     print(f'オープン可能カメラID:{result_list}')
     #カメラ画像を表示
-    webcam.show_camera_stream(result_list[0])
+    #webcam.show_camera_stream(result_list[0])
 
 
 if __name__ == '__main__':
